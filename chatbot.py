5c87c886eb4010f2477cbcbe361bf191db40ace6
myidispg <myidispg@gmail.com>
2019-01-22 20:51:39 +0530

    Extracted conversations and lines

--- chatbot.py
+++ chatbot.py
@@ -17,3 +17,23 @@ import time
 
 
 #########- Part 1-DATA PREPROCESSING ########
+
+# Import the dataset
+lines = open('cornell movie-dialogs corpus/movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+conversations = open('cornell movie-dialogs corpus/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# Creating a dictionary that maps each ine with its id
+id2line = {}
+
+for line in lines:
+    _line = line.split('+++$+++')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+# Creating a list of all the conversations
+conversations_ids = []
+
+for conversation in conversations[:-1]:
+    _conversation = conversation.split('+++$+++')[-1][2:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+    
