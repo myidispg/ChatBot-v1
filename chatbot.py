2fdf485725929e12bd616095deacda3eac8fcacb
myidispg <myidispg@gmail.com>
2019-01-22 20:33:54 +0530

    Working on data preprocessing

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 20:06:46 2019
+
+@author: myidispg
+"""
+
+# Building a chatbot with Deep NLP
+
+
+# Importing the libraries
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
+
+#########- Part 1-DATA PREPROCESSING ########
