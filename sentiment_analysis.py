645fadad78fe14ef02d56a52163f250c021c9284
myidispg <myidispg@gmail.com>
2019-01-25 20:49:36 +0530

    Working on data preprocessing

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,3 +6,24 @@ Created on Fri Jan 25 20:23:37 2019
 @author: myidispg
 """
 
+import re
+
+reviews_train = []
+for line in open('../Datasets/movie_data/full_train.txt', 'r'):
+    reviews_train.append(line.strip())
+    
+reviews_test = []
+for line in open('../Datasets/movie_data/full_test.txt', 'r'):
+    reviews_test.append(line.strip())
+    
+REPLACE_NO_SPACE = re.compile("(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])")
+REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+
+def preprocess_reviews(reviews):
+    reviews = [REPLACE_NO_SPACE.sub("", line.lower()) for line in reviews]
+    reviews = [REPLACE_WITH_SPACE.sub(" ", line) for line in reviews]
+    
+    return reviews
+
+reviews_train_clean = preprocess_reviews(reviews_train)
+reviews_test_clean = preprocess_reviews(reviews_test)
