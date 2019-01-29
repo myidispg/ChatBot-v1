0aaf04805f4c0836bb937b5fff4d81037812ebd2
myidispg <myidispg@gmail.com>
2019-01-29 18:53:01 +0530

    Working on lematization now

--- NLP Course/01-NLP-Python-Basics/stemmer.py
+++ NLP Course/01-NLP-Python-Basics/stemmer.py
@@ -7,3 +7,17 @@ Created on Tue Jan 29 18:46:27 2019
 """
 
 import nltk
+from nltk.stem.porter import PorterStemmer
+
+p_stemmer = PorterStemmer()
+
+words = ['run', 'runner', 'ran', 'runs', 'easily', 'fairly', 'fairness']
+for word in words:
+    print(word + '------>' + p_stemmer.stem(word))
+    
+from nltk.stem.snowball import SnowballStemmer
+
+s_stemmer = SnowballStemmer(language='english')
+
+for word in words:
+    print(word + '------->' + s_stemmer.stem(word))
