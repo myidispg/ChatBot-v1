6a806c46f58d3c292559f48264b4ef01011689ac
myidispg <myidispg@gmail.com>
2019-03-02 15:25:05 +0530

    CHanged gitignore to reduce commit size

--- neural_machine_translation/translation.py
+++ neural_machine_translation/translation.py
@@ -5,59 +5,19 @@ Created on Sat Mar  2 14:24:34 2019
 
 @author: myidispg
 """
+# Load doc into memory
+def load_doc(filename):
+    file = open(filename, mode='rt', encoding='utf-8')
+    text = file.read()
+    file.close()
+    return text
 
-from pickle import load
-import numpy as np
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.utils.vis_utils import plot_model
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import Embedding
-from keras.layers import RepeatVector
-from keras.layers import TimeDistributed
-from keras.callbacks import ModelCheckpoint
+# split a loaded document into sentences
+def to_sentences(doc):
+    return doc.strip().split('\n')
 
-# load a clean dataset
-def load_clean_dataset(filename):
-    return load(open(filename, 'rb'))
+# shortest and longest sentences
+def sentence_lengths(sentences):
+    lengths = [len(s.split()) for s in sentence]
+    return min(lengths), max(lengths)
 
-# fit a tokenizer
-def create_a_tokenizer(lines):
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(lines)
-    return tokenizer
-
-# max sentence length
-def max_length(lines):
-    return max(len(line.split()) for line in lines)
-
-# encode and pad sequences
-def encode_sequences(tokenizer, length, lines):
-    # integer encode sequences
-    X = tokenizer.text_to_sequences(lines)
-    # pad sequences with a 0 value
-    X = pad_sequences(X, maxlen=length, padding='post')
-    return X
-
-# one hot encode target sequence
-def encode_output(sequences, vocab_size):
-    ylist = list()
-    for sequence in sequences:
-        encoded = to_categorical(sequence, num_classes=vocab_size)
-        ylist.append(encoded)
-    y = np.array(ylist)
-    y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
-    return y
-
-# define NMT model
-def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
-    model = Sequential()
-    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
-    model.add(LSTM(n_units))
-    model.add(RepeatVector(tar_timesteps))
-	model.add(LSTM(n_units, return_sequences=True))
-	model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
-	return model
