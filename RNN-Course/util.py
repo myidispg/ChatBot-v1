10f96cd55e222d355c1eb5bfc2abea7e643ba652
myidispg <myidispg@gmail.com>
2019-03-07 20:24:16 +0530

    Updated utils for poetry helper functions

--- RNN-Course/util.py
+++ RNN-Course/util.py
@@ -34,3 +34,33 @@ def all_parity_pairs(nbit):
                 X[ii,j] = 1
         Y[ii] = X[ii].sum() % 2
     return X, Y
+
+# unfortunately Python 2 and 3 translates work differently
+def remove_punctuation_2(s):
+    return s.translate(None, string.punctuation)
+
+def remove_punctuation_3(s):
+    return s.translate(str.maketrans('','',string.punctuation))
+
+if sys.version.startswith('2'):
+    remove_punctuation = remove_punctuation_2
+else:
+    remove_punctuation = remove_punctuation_3
+
+def get_robert_frost():
+    word2idx = {'START': 0, 'END': 1}
+    current_idx = 2
+    sentences = []
+    for line in open('../hmm_class/robert_frost.txt'):
+        line = line.strip()
+        if line:
+            tokens = remove_punctuation(line.lower()).split()
+            sentence = []
+            for t in tokens:
+                if t not in word2idx:
+                    word2idx[t] = current_idx
+                    current_idx += 1
+                idx = word2idx[t]
+                sentence.append(idx)
+            sentences.append(sentence)
+    return sentences, word2idx
