af071942cc7436f1842503c45d0b0896f10ebeb1
myidispg <myidispg@gmail.com>
2019-03-14 11:48:42 +0530

    Completed word embeddings visualization

--- RNN-Course/visualize_embeddings.py
+++ RNN-Course/visualize_embeddings.py
@@ -8,7 +8,7 @@ Created on Tue Mar 12 20:44:25 2019
 
 import json
 import numpy as np
-import matploylib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA, TruncatedSVD
 
@@ -18,14 +18,14 @@ def main(we_file='word_embeddings.npy', w2i_file='wikipedia_word2idx.json', Mode
     with open(w2i_file) as f:
         word2idx = json.load(f)
         
-    idx2word = {v: k for k, v in iter(word2idx)}
+    idx2word = {word2idx[k]: k for k in word2idx}
     
     model = Model()
     Z = model.fit_transform(We)
-    plt.scatter(Z[:0], Z[:, 1])
+    plt.scatter(Z[:, 0], Z[:, 1])
     for i in range(V):
         plt.annotate(s=idx2word[i], xy=(Z[i,0], Z[i, 1]))
     plt.show()
     
-main(we_file='gru_nonorm_part1_word_embeddings.npy',w2i_file='gru_nonorm_part1_wikipedia_word2idx.json', model=TSNE)
+main(we_file='gru_nonorm_part1_word_embeddings.npy',w2i_file='gru_nonorm_part1_wikipedia_word2idx.json', Model=TSNE)
 
