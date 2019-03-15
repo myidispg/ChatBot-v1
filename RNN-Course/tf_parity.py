3be98d1b445d6fd2595ed09e4591187b53a477e5
myidispg <myidispg@gmail.com>
2019-03-15 15:23:06 +0530

    Working on TF implementation of Parity Problem.

--- RNN-Course/tf_parity.py
+++ RNN-Course/tf_parity.py
@@ -16,4 +16,15 @@ from tensorflow.contrib.rnn import BasicRNNCell, GRUCell
 from sklearn.utils import shuffle
 from util import all_parity_pairs, all_parity_pairs_with_sequence_labels
 
-X, Y = all_parity_pairs_with_sequence_labels(12)
+def x2sequence(x, T, D, batch_size):
+    # Permuting batch_size, and n_steps
+    x = tf.transpose(x, (1, 0, 2)) # NxTxD => TxNxD
+    # reshaping to (n_steps*batch_size, n_input)
+    x = tf.reshape(x, (T*batch_size, D))
+    # Split to get a list of n_steps tensors of shape(batch_size, n_input)
+    x = tf.split(x, T)
+    
+    return x
+
+X, Y = all_parity_pairs_with_sequence_labels(10)
+x = x2sequence(X, 10, 1, 1100)
