d309547e651a023a3acc125111c447df7eec49f3
myidispg <myidispg@gmail.com>
2019-03-19 18:20:20 +0530

    Working on tf implementation of parity problem

--- RNN-Course/tf_parity.py
+++ RNN-Course/tf_parity.py
@@ -14,7 +14,7 @@ from tensorflow.contrib.rnn import static_rnn as get_rnn_output
 from tensorflow.contrib.rnn import BasicRNNCell, GRUCell
 
 from sklearn.utils import shuffle
-from util import all_parity_pairs, all_parity_pairs_with_sequence_labels
+from util import init_weight, all_parity_pairs, all_parity_pairs_with_sequence_labels
 
 def x2sequence(x, T, D, batch_size):
     # Permuting batch_size, and n_steps
@@ -26,5 +26,59 @@ def x2sequence(x, T, D, batch_size):
     
     return x
 
+class SimpleRNN:
+    
+    def __init__(self, M):
+        self.M = M # Hidden layer size.
+        
+    def fit(self, X, Y, batch_size=20, learning_rate=0.1, mu=0.9, activation=tf.nn.sigmoid, epochs=100, show_fig=False):
+        N, T, D = X.shape # X is of size N x T(n) x D
+        K = len(set(Y.flatten())) # get the number of unique elements in Y.
+        M = self.M
+        self.f = activation
+        
+        # initial weights
+        # note: Wx, Wh, bh are all parts of RNN cell and will be created.
+        Wo = init_weight(M, K).astype(np.float32)
+        bo = np.zeros(K, dtype=np.float32)
+        
+        # make them tf variables
+        self.Wo = tf.Variable(Wo)
+        self.bo = tf.Variable(bo)
+        
+        # tf Graph input
+        tfX = tf.placeholder(tf.float32, shape=(batch_size, T, D), name='inputs')
+        tfY = tf.placeholder(tf.int64, shape=(batch_size, T), name='targets')
+        
+        # turn tfX into a sequence, e.g. T tensors all of size (batch_sz, D)
+        sequenceX = x2sequence(tfX, T, D, batch_size)
+        
+        # create the simple RNN unit
+        rnn_unit = BasicRNNCell(num_units = self.M, activation=self.f)
+        
+        # get rnn cell output
+        outputs, states = get_rnn_output(rnn_unit, sequenceX, dtype=tf.float32)
+        
+        # outputs are now of size (T, batch_size, M)
+        # so make it (batch_size, T, M)
+        outputs = tf.transpose(outputs, (1, 0, 2))
+        outputs = tf.reshape(outputs, (T*batch_size, M))
+        
+        # Linear activation using rnn inner looplast output
+        logits = tf.matmul(outputs, self.Wo) * self.bo
+        predict_op = tf.argmax(logits, 1)
+        targets = tf.reshape(tfY, (T*batch_size, ))
+        
+        cost_op = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(
+                        logits=logits,
+                        labels=targets)
+                )
+        train_op = tf.train.MomentumOptimizer(learning_rate, momentum=mu).minimize(cost_op)
+        
+        costs = []
+        n_batches = N // batch_size
+    
+    
 X, Y = all_parity_pairs_with_sequence_labels(10)
 x = x2sequence(X, 10, 1, 1100)
