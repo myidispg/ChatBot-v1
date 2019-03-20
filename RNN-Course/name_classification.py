6ee39e5c6ad9ea4094a5573348ff46c217cf954f
myidispg <myidispg@gmail.com>
2019-03-20 15:53:33 +0530

    Started training

--- RNN-Course/name_classification.py
+++ RNN-Course/name_classification.py
@@ -112,13 +112,114 @@ class RNN(nn.Module):
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 
+"""
+To run a step of this network we need to pass an input
+ (in our case, the Tensor for the current letter) 
+ and a previous hidden state (which we initialize as zeros at first).
+ We’ll get back the output (probability of each language) and a next
+ hidden state (which we keep for the next step).
+"""
+input = letterToTensor('A')
+hidden = torch.zeros(1, n_hidden)
+
+output, next_hidden = rnn.forward(input, hidden)
         
-        
+input = lineToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
 
+output, next_hidden = rnn(input[0], hidden)
+print(output)   
 
+# Training.
 
+# Interpret the output of the network, which we know to be a likelihood of each category.
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
 
+print(categoryFromOutput(output))
 
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+"""
+Training the Network
+Now all it takes to train this network is show it a bunch of examples,
+ have it make guesses, and tell it if it’s wrong.
+
+For the loss function nn.NLLLoss is appropriate, since the last layer of the RNN is nn.LogSoftmax.
+"""
+
+criterion = nn.NLLLoss() # Negative Log Likelihood
+
+learning_rate = 0.005
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    
+    rnn.zero_grad()
+    
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+    
+    loss = criterion(output, category_tensor)
+    loss.backward()
+    
+     # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
+    return output, loss.item()
+
+import time
+import math
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s/60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+    
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+    
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
 
 
 
