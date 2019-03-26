06e4545891f1c322ee5a66d4f9163b7c78a42d19
myidispg <myidispg@gmail.com>
2019-03-26 20:33:15 +0530

    Completed evaluation. The model is performing great

--- Pytorch Projects/fra_eng_translation.py
+++ Pytorch Projects/fra_eng_translation.py
@@ -344,7 +344,7 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
             plot_losses.append(plot_loss_avg)
             plot_loss_total = 0
 
-    showPlot(plot_losses)
+    showPlot(plot_loss_total)
     
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -360,6 +360,51 @@ def showPlot(points):
     ax.yaxis.set_major_locator(loc)
     plt.plot(points)
     
+# EVALUATION
+def evaluate(encoder, decoder, sentence, max_length = MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+        
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+
+        decoder_hidden = encoder_hidden
+
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            else:
+                decoded_words.append(output_lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words, decoder_attentions[:di + 1]
+    
+def evaluateRandomly(encoder, decoder, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = evaluate(encoder, decoder, pair[0])
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
 
 EMBEDDING_DIM = 6
 HIDDEN_DIM = 256
@@ -370,6 +415,9 @@ criterion = nn.NLLLoss()
 
 trainIters(encoder, attn_decoder, 75000, print_every=5000)
 print(encoder.state_dict().keys())
+
+evaluateRandomly(encoder, attn_decoder)
+
 
 # Save the encoder and decoder network for reuse later.
 torch.save(encoder.state_dict(), 'encoder_seq2seq_with_attention.pth')
